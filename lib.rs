7a66d353ef76b6e3501ea58b1df99e1d50bd0056
Weiliang Li <to.be.impressive@gmail.com>
2019-11-22 14:39:58 +0900

    add token manipulation

--- lib.rs
+++ lib.rs
@@ -2,8 +2,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::new_ret_no_self)]
 
-// use ink_core::env2::call::FromAccountId;
-use ink_core::storage;
+use ink_core::env2::call::FromAccountId;
+use ink_core::{memory::vec::Vec, storage};
 use ink_lang2 as ink;
 use sha2::{Digest, Sha256};
 
@@ -15,7 +15,8 @@ type Bytes = [u8; 32];
 mod htlc {
     #[ink(storage)]
     struct Htlc {
-        // token: storage::Value<Mintable>,
+        token: storage::Value<Mintable>,
+        amount: storage::Value<Balance>,
         buyer: storage::Value<AccountId>,
         seller: storage::Value<AccountId>,
         expiration_in_ms: storage::Value<Moment>,
@@ -24,13 +25,32 @@ mod htlc {
 
     impl Htlc {
         #[ink(constructor)]
-        fn new(&mut self, buyer: AccountId, expiration_in_ms: Moment, secret_hash: Bytes) {
-            let seller = self.env().caller();
+        fn new(
+            &mut self,
+            token_account: AccountId,
+            amount: Balance,
+            buyer: AccountId,
+            expiration_in_ms: Moment,
+            secret_hash: Bytes,
+        ) {
+            self.token.set(Mintable::from_account_id(token_account));
+
             self.buyer.set(buyer);
-            self.seller.set(seller);
+            self.seller.set(token_account);
+
+            self.amount.set(amount);
+
+            let now = self.env().now_in_ms();
+            assert!(expiration_in_ms > now);
             self.expiration_in_ms.set(expiration_in_ms);
+
             self.secret_hash.set(secret_hash);
-            // self.token.set(Mintable::from_account_id(address));
+        }
+
+        #[ink(message)]
+        fn balance(&self) -> Balance {
+            let this = self.env().address();
+            self.token.balance_of(this)
         }
 
         #[ink(message)]
@@ -53,35 +73,56 @@ mod htlc {
             *self.secret_hash
         }
 
-        // #[ink(message)]
-        // fn test_sha256(&self, input: Vec<u8>) -> Bytes {
-        //     self.sha256(input.as_slice())
-        // }
+        #[ink(message)]
+        fn is_valid(&self) -> bool {
+            self._is_valid()
+        }
 
         #[ink(message)]
-        fn claim(&mut self, secret: Bytes) -> bool {
+        fn test_sha256(&self, input: Vec<u8>) -> Bytes {
+            self._sha256(&input)
+        }
+
+        #[ink(message)]
+        fn claim(&mut self, secret: Vec<u8>) -> bool {
+            assert!(self._is_valid(), "no enough balance");
+
             // check timestamp
             let now = self.env().now_in_ms();
             assert!(now <= *self.expiration_in_ms);
 
             // check sha256(secret) == secret_hash
-            assert_eq!(self.sha256(&secret), *self.secret_hash);
+            assert_eq!(self._sha256(&secret), *self.secret_hash);
 
-            // transfer
+            // transfer contract's amount to buyer
+            let buyer = *self.buyer;
+            let amount = *self.amount;
+            self.token.transfer(buyer, amount);
+
+            // TODO: suicide
             true
         }
 
         #[ink(message)]
         fn refund(&mut self) -> bool {
-            // check tiemstamp
+            // check timestamp
             let now = self.env().now_in_ms();
             assert!(now > *self.expiration_in_ms);
 
-            // transfer
+            // burn contract's token
+            let amount = *self.amount;
+            self.token.burn(amount);
+
+            // TODO: suicide
             true
         }
 
-        fn sha256(&self, input: &[u8]) -> Bytes {
+        fn _is_valid(&self) -> bool {
+            let this = self.env().address();
+            self.token.balance_of(this) == *self.amount
+        }
+
+        fn _sha256(&self, input: &[u8]) -> Bytes {
             let mut hasher = Sha256::new();
 
             hasher.input(input);
